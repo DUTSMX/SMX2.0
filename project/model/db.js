b6489ef9717a1e638a57f1f4eac623b992e1ebd5
18742021291 <wall1003@qq.com>
2017-08-20 22:24:38 +0800

    student_detail

---
+++ project/model/db.js
@@ -0,0 +1,17 @@
+var Sequelize = require('sequelize');
+var sequelize=new Sequelize(
+    'smx_test',
+    'cdb_outerroot',
+    'smxsjk123456',
+    {
+        host: '59432c47c3382.bj.cdb.myqcloud.com',
+        port: 4243,
+        dialect: 'mysql',
+        define: {
+            underscored: false,
+            timestamps: false,
+            paranoid: true
+        }
+    }
+);
+exports.sequelize =  sequelize;
--- project/routes/index.js
+++ project/routes/index.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
+var db =require("../model/db")
+var student =require("../model/student")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/student_detail',function(req,res,next){
-  res.render('student_detail')
+    /*student.findOne({'where':{userId:34}}).then(function (ret) {
+        ret.getUser().then(function (ret1) {
+            console.log(JSON.stringify({info:ret1,infos:ret}))
+            res.render('student_detail',{info:ret1,infos:ret});
+        })
+    })*/
+    var studentId = 16;
+    var sql = "SELECT a.userName, " +
+        "a.userAge, " +
+        "s.school, " +
+        "s.personalSign " +
+        "FROM  student s " +
+        "JOIN account a ON a.userId = s.userId  " +
+        "WHERE s.studentId = " + studentId;
+    db.sequelize.query(sql).then(function (ret) {
+        console.log(JSON.stringify(ret));
+        console.log(JSON.stringify({date:ret[0][0]}));
+        res.render('student_detail',{data:ret[0][0]});
+    })
 });
 
 router.get('/student',function(req,res,next){
