84cfd591f9e2d16c62b4d2a4cef580c28d206c2a
coding-j <401236678@qq.com>
2017-08-21 14:03:03 +0800

    updata

--- project/routes/index.js
+++ project/routes/index.js
@@ -14,6 +14,25 @@ router.get('/student',function(req,res,next){
   res.render('student_course');
 });
 router.get('/course_re',function(req,res,next){
-    res.render('course_re')
+    res.render('course_re');
 });
+
+// router.post("/postHope",function (req,res) {
+//     series.joinSeries.findAll({where:{templateId:req.body.templateId,studentId:req.body.studentId}}).then(function (joinList) {
+//         if (joinList.length == 0){
+//             series.joinSeries.create({
+//                 templateId:req.body.templateId,
+//                 studentId:req.body.studentId,
+//                 classType:req.body.classType,
+//                 hopeTime:req.body.hopeTime,
+//                 other:req.body.other
+//             }).then(function () {
+//                 res.send({status:true,desc:"提交成功"})；
+//             })
+//         }else{
+//             res.send({status:false,desc:"您已经报名该课程"})
+//         }
+//     })
+//
+// })
 module.exports = router;
